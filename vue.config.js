ea0a52e9894594cbc3dd061d3c6880657be98e0c
Bobby <944831575@qq.com>
2019-05-13 17:17:36 +0800

    :arrow_up: 版本升级

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
 // vue.config.js
-var path = require('path')
+var path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, './', dir)
+  return path.join(__dirname, './', dir);
 }
 
 module.exports = {
-  baseUrl: '',
+  publicPath: '/',
   outputDir: 'dist',
   // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
   assetsDir: 'assets',
   // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-  indexPath: "index.html",
+  indexPath: 'index.html',
   chainWebpack: config => {
     config.module
       .rule('svg')
       .exclude.add(resolve('src/icons'))
-      .end()
+      .end();
 
     config.module
       .rule('icons')
@@ -27,6 +27,6 @@ module.exports = {
       .loader('svg-sprite-loader')
       .options({
         symbolId: 'icon-[name]'
-      })
+      });
   }
-}
+};
