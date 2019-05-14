d443b227a421c03af438bd712fc73cab1f815564
Bobby <944831575@qq.com>
2019-05-14 17:31:33 +0800

    :lipstick: login change

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,53 @@
 <template>
-  <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-
+  <div class="form-container">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="form-group">
       <div class="title-container">
-        <h3 class="title">admin</h3>
+        <h3 class="title">Admin</h3>
       </div>
 
       <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
         <el-input type="text" autocomplete="on" placeholder="用户名" v-model="loginForm.username">
+          <template slot="prepend">
+            <svg-icon icon-class="user" />
+          </template>
         </el-input>
       </el-form-item>
 
       <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input :type="passwordType" autocomplete="on" placeholder="密码" v-model="loginForm.password">
+        <el-input
+          :type="passwordType"
+          autocomplete="on"
+          placeholder="密码"
+          v-model="loginForm.password"
+        >
+          <template slot="prepend">
+            <svg-icon icon-class="password" />
+          </template>
         </el-input>
         <span class="show-pwd" @click="showPwd">
-          <svg-icon icon-class="eye" />
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
+      <el-form-item>
+        <el-button
+          :loading="loading"
+          type="primary"
+          style="width:100%;"
+          @click.native.prevent="handleLogin"
+          >登录</el-button
+        >
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" style="width: 100%;" @click.native.prevent="gotoRegister">
+          注册
+        </el-button>
+      </el-form-item>
+
+      <div class="links">
+        <router-link to="/forgetPwd">忘记密码？</router-link>
+      </div>
     </el-form>
   </div>
 </template>
@@ -116,6 +138,7 @@ export default class Login extends Vue {
     ]
   }
 
+  // 是否显示密码
   private showPwd() {
     if (this.passwordType === 'password') {
       this.passwordType = 'text'
@@ -145,93 +168,3 @@ export default class Login extends Vue {
   }
 }
 </script>
-
-<style lang="scss">
-$bg: #283443;
-$light_gray: #eee;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-    &::first-line {
-      color: $light_gray;
-    }
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-      &:-webkit-autofill {
-        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-
-.login-container {
-  position: fixed;
-  height: 100%;
-  width: 100%;
-  background-color: $bg;
-  .login-form {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 520px;
-    max-width: 100%;
-    padding: 35px 35px 15px 35px;
-    margin: 120px auto;
-    .title-container {
-      position: relative;
-      .title {
-        font-size: 26px;
-        color: $light_gray;
-        margin: 0px auto 40px auto;
-        text-align: center;
-        font-weight: bold;
-      }
-      .set-language {
-        color: #fff;
-        position: absolute;
-        top: 5px;
-        right: 0px;
-      }
-    }
-    .svg-container {
-      padding: 6px 5px 6px 15px;
-      color: $dark_gray;
-      vertical-align: middle;
-      width: 30px;
-      display: inline-block;
-    }
-  }
-}
-</style>
-
